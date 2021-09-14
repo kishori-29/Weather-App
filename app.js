f4d8bb883d2657269c388d6c26877aab82d00800
kishori-29 <78212515+kishori-29@users.noreply.github.com>
2021-09-14 23:30:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let weather={
+   apikey:"f6b17387fa622aff8bc4a3bd043728ea",
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + city 
+        +"&units=metric&appid=" + this.apikey
+        )
+        .then((response)=>response.json())
+        .then((data)=>this.displayWeather(data));
+    },
+    displayWeather:function(data){
+        const{ name }=data;
+
+        const {icon,description}=data.weather[0];
+        const{ temp, humidity }= data.main;
+        const{speed}=data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText="Weather in "+name;
+        document.querySelector(".icon").src= "https://openweathermap.org/img/wn/"+icon+".png";
+        document.querySelector(".description").innerText =description;
+        document.querySelector(".temp").innerText =temp+"°C";
+        document.querySelector(".humidity").innerText ="humidity: "+humidity+"%";
+        document.querySelector(".wind").innerText ="wind speed: "+speed+"km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".searchbar").value);
+    },
+};
+
+document
+    .querySelector(".search button")
+    .addEventListener("click", function(){
+        weather.search();
+    });
+document.querySelector(".searchbar").addEventListener("keyup", function(event){
+    if(event.key=="Enter"){
+        weather.search();
+    }
+});
+weather.fetchWeather("Ahmednagar");
